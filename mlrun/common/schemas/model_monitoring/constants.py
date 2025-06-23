c8cc92a1b063b978f5979c35e9f6212a6a53cffd
Alex Toker <alex_toker@mckinsey.com>
2025-06-23 16:45:59 +0000

    [Model Monitoring] Implementation of Timescaledb

--- mlrun/common/schemas/model_monitoring/constants.py
+++ mlrun/common/schemas/model_monitoring/constants.py
@@ -271,6 +271,7 @@ class EventKeyMetrics:
 class TSDBTarget(MonitoringStrEnum):
     V3IO_TSDB = "v3io-tsdb"
     TDEngine = "tdengine"
+    TimescaleDB = "postgresql"
 
 
 class ProjectSecretKeys:
@@ -349,6 +350,13 @@ class V3IOTSDBTables(MonitoringStrEnum):
 
 
 class TDEngineSuperTables(MonitoringStrEnum):
+    APP_RESULTS = "app_results"
+    METRICS = "metrics"
+    PREDICTIONS = "predictions"
+    ERRORS = "errors"
+
+
+class TimescaleDBTables(MonitoringStrEnum):
     APP_RESULTS = "app_results"
     METRICS = "metrics"
     PREDICTIONS = "predictions"
